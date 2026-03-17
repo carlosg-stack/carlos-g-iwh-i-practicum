f0c6db778d0e62693bc6de76a374b5b1472fc620
Carlos Giron <carlosgironr@gmail.com>
2026-03-16 21:11:39 -0600

    Final version of HubSpot Practicum

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -7,65 +8,56 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// * Please DO NOT INCLUDE the private app access token in your repo.
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS_TOKEN;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data.
+app.get('/', async (req, res) => {
+    const customObjectID = '2-226859943';
+    const endpoint = `https://api.hubspot.com/crm/v3/objects/${customObjectID}?properties=name,type,character_trait`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     }
     try {
-        const resp = await axios.get(contacts, { headers });
+        const resp = await axios.get(endpoint, { headers });
         const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        res.render('homepage', { title: 'Custom Objects | HubSpot Foundations', data });      
     } catch (error) {
-        console.error(error);
+        console.error("Error retrieving data:", error.response ? error.response.data : error.message);
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
+// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data.
+app.get('/update-cobj', async (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I' });
+});
+
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data.
+app.post('/update-cobj', async (req, res) => {
+    const customObjectID = '2-226859943';
+    const endpoint = `https://api.hubspot.com/crm/v3/objects/${customObjectID}`;
+    
     const update = {
         properties: {
-            "favorite_book": req.body.newVal
+            "name": req.body.new_name,
+            "type": req.body.new_type,
+            "character_trait": req.body.new_trait
         }
     }
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
 
     try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
+        await axios.post(endpoint, update, { headers });
+        res.redirect('/');
     } catch(err) {
-        console.error(err);
+        console.error("Error creating record:", err.response ? err.response.data : err.message);
     }
-
 });
-*/
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
